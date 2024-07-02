e39649de66444a21a2084f5f66b9c19c82d24034
dayerzz <dsy2004@list.ru>
2024-07-02 21:28:19 +0300

    add second table for status for vacancy

--- src/models.py
+++ src/models.py
@@ -1,8 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -20,6 +19,18 @@ class Vacancy(Base):
     salary_to = Column(Integer, nullable=True)
     currency = Column(String, nullable=True)
     url = Column(String)
+    status_id = Column(Integer, ForeignKey('vacancy_status.id'))
+
+    status = relationship("VacancyStatus", back_populates="vacancies")
+
+
+class VacancyStatus(Base):
+    __tablename__ = 'vacancy_status'
+
+    id = Column(Integer, primary_key=True, index=True)
+    status = Column(String, unique=True)
+
+    vacancies = relationship("Vacancy", back_populates="status")
 
 
 engine = create_engine(DATABASE_URL)
